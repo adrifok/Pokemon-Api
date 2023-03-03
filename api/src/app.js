032681125364eb6cce04f3b3c3a8d418a63ba89e
adrifok <adrifok@gmail.com>
2023-03-02 18:34:03 -0600

    10th commit from vscode (deply routes error)

--- api/src/app.js
+++ api/src/app.js
@@ -15,9 +15,7 @@ server.use(express.json({ limit: '50mb' }));
 server.use(cookieParser());
 server.use(morgan('dev'));
 server.use((req, res, next) => {
-  res.header('Access-Control-Allow-Origin', 'https://pokemon-api-adrifok.vercel.app'); // update to match the domain you will make the request from
-                                            
-                                              //'https://pokemon-api-phi-seven.vercel.app'
+  res.header('Access-Control-Allow-Origin', 'https://pokemon-api-phi-seven.vercel.app'); // update to match the domain you will make the request from
   res.header('Access-Control-Allow-Credentials', 'true');
   res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
   res.header('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, DELETE');
