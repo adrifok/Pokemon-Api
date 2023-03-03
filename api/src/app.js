227bb7d5b003c0eb004b43cf983bd6389c91a5c0
adrifok <adrifok@gmail.com>
2023-03-02 18:30:07 -0600

    9th commit form vscode (url correction)

--- api/src/app.js
+++ api/src/app.js
@@ -15,7 +15,7 @@ server.use(express.json({ limit: '50mb' }));
 server.use(cookieParser());
 server.use(morgan('dev'));
 server.use((req, res, next) => {
-  res.header('Access-Control-Allow-Origin', 'https://pokemon-api-adrifok.vercel.app/'); // update to match the domain you will make the request from
+  res.header('Access-Control-Allow-Origin', 'https://pokemon-api-adrifok.vercel.app'); // update to match the domain you will make the request from
                                             
                                               //'https://pokemon-api-phi-seven.vercel.app'
   res.header('Access-Control-Allow-Credentials', 'true');
